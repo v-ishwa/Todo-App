ae72f0ce8796712eaa1cede549eb35973a50fc9d
v-ishwa <m.viswa001@gmail.com>
2025-12-01 09:09:14 +0530

    final commit

--- components/TodoInput.tsx
+++ components/TodoInput.tsx
@@ -1,52 +1,61 @@
-import { createHomeStyles } from '@/assets/styles/home.styles';
-import { api } from '@/convex/_generated/api';
-import useTheme from '@/hooks/useTheme';
-import { Ionicons } from '@expo/vector-icons';
-import { useMutation } from 'convex/react';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react';
-import { Alert, TextInput, TouchableOpacity, View } from 'react-native';
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation } from "convex/react";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { Alert, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function TodoInput() {
-    const { colors } = useTheme();
-    const homeStyles = createHomeStyles(colors);
+  const { colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
 
-    const [newTodo, setNewTodo] = useState('');
-    const addTodo = useMutation(api.todos.addTodo);
+  const [newTodo, setNewTodo] = useState("");
+  const addTodo = useMutation(api.todos.addTodo);
 
-    const handleAddTodo = async () => {
-        if (newTodo.trim()) {
-            try {
-                await addTodo({ text: newTodo.trim() })
-                setNewTodo('');
-            } catch (error) {
-                console.log("Error adding a todo", error);
-                Alert.alert("failed to add todo");
-            }
-        }
+  const handleAddTodo = async () => {
+    if (newTodo.trim()) {
+      try {
+        await addTodo({ text: newTodo.trim() });
+        setNewTodo("");
+      } catch (error) {
+        console.log("Error adding a todo", error);
+        Alert.alert("failed to add todo");
+      }
     }
+  };
 
-    return (
-        <View style={homeStyles.inputSection}>
-            <View style={homeStyles.inputWrapper}>
-                <TextInput
-                    style={homeStyles.input}
-                    placeholder="What needs to be done?"
-                    value={newTodo}
-                    onChangeText={setNewTodo}
-                    onSubmitEditing={handleAddTodo}
-                    placeholderTextColor={colors.textMuted}
-                />
+  return (
+    <View style={homeStyles.inputSection}>
+      <View style={homeStyles.inputWrapper}>
+        <TextInput
+          style={homeStyles.input}
+          placeholder="What needs to be done?"
+          value={newTodo}
+          onChangeText={setNewTodo}
+          onSubmitEditing={handleAddTodo}
+          placeholderTextColor={colors.textMuted}
+        />
 
-                <TouchableOpacity onPress={handleAddTodo} activeOpacity={0.8} disabled={!newTodo.trim()}>
-                    <LinearGradient colors={
-                        newTodo.trim() ? colors.gradients.primary : colors.gradients.muted}
-                        style={[homeStyles.addButton, !newTodo.trim() && homeStyles.addButtonDisabled]}
-                    >
-                        <Ionicons name='add' size={24} color={"#ffffff"} />
-                    </LinearGradient>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+        <TouchableOpacity
+          onPress={handleAddTodo}
+          activeOpacity={0.8}
+          disabled={!newTodo.trim()}
+        >
+          <LinearGradient
+            colors={
+              newTodo.trim() ? colors.gradients.primary : colors.gradients.muted
+            }
+            style={[
+              homeStyles.addButton,
+              !newTodo.trim() && homeStyles.addButtonDisabled,
+            ]}
+          >
+            <Ionicons name="add" size={24} color={"#ffffff"} />
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
--- convex/schema.ts
+++ convex/schema.ts
@@ -2,8 +2,8 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    todos: defineTable({
-        text: v.string(),
-        isCompleted: v.boolean(),
-    })
-})
+  todos: defineTable({
+    text: v.string(),
+    isCompleted: v.boolean(),
+  }),
+});
